3650be8cb864309acc77574ef82adb2b3eda4376
Raynos <raynos2@gmail.com>
2012-01-23 14:14:19 +0000

    0.2 refactor

--- test/ee-test.js
+++ test/ee-test.js
@@ -9,8 +9,6 @@ suite('EventEmitter', function () {
             "EventEmitter.removeListener does not exist");
         assert(EventEmitter.emit, "EventEmitter.emit does not exist");
         assert(EventEmitter.once, "EventEmitter.once does not exist");
-        assert(EventEmitter.removeAllListeners,
-            "EventEmitter.removeAllListeners does not exist");
     });
 
     test('EventEmitter.on', function () {
@@ -31,27 +29,6 @@ suite('EventEmitter', function () {
             
         ee.on('ev', increment);
         ee.removeListener('ev', increment);
-
-        ee.emit('ev');
-        assert.equal(counter, 0, "counter is not correct");
-
-        function increment() {
-            counter++;
-        }
-    });
-
-    test('EventEmitter.removeAllListeners', function () {
-        var ee = instance(),
-            counter = 0;
-            
-        ee.on('ev', increment);
-        ee.removeAllListeners('ev');
-
-        ee.emit('ev');
-        assert.equal(counter, 0, "counter is not correct");
-
-        ee.on('ev', increment);
-        ee.removeAllListeners();
 
         ee.emit('ev');
         assert.equal(counter, 0, "counter is not correct");
@@ -84,27 +61,6 @@ suite('EventEmitter', function () {
 
         assert.equal(counter, 1, "counter is not correct");
     });
-
-    test('namespaced events', function () {
-        var ee = instance(),
-            counter = 0;
-
-        ee.on('ev.namespace', function () {
-            counter++; 
-        });
-
-        ee.emit('ev.*');
-
-        assert.equal(counter, 1, "counter is not correct");
-
-        ee.on('ev2.*', function () {
-            counter++;
-        });
-
-        ee.emit('ev2.namespace');
-
-        assert.equal(counter, 2, "counter is not correct");
-    })
 });
 
 function instance() {
